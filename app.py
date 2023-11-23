b1cffe4849110fbe7a361835b2952298a7033f83
holysoles <holysoles97@gmail.com>
2023-11-23 03:58:25 +0000

    parse code snippets

--- app.py
+++ app.py
@@ -8,12 +8,23 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     yaml_path = './posts/'
+    code_snippet_path = './code_snippets/'
     yaml_files = [join(yaml_path, file) for file in listdir(yaml_path) if isfile(join(yaml_path, file))]
     post_array = []
     yaml_files.sort(reverse=True)
     for yaml_file in yaml_files:
         with open(yaml_file, 'r') as file:
-            post_array.append(yaml.safe_load(file))
+            post_data = yaml.safe_load(file)
+        
+        # parse and preload code snippet files
+        if post_data.get('body'):
+            for body in post_data['body']:
+                if body.get('code'):
+                    with open(join(code_snippet_path, body['code']), 'r') as file:
+                        code_snippet = file.read()
+                        body['code'] = code_snippet
+        
+        post_array.append(post_data)
     return render_template('blog.html', post_array=post_array)
 
 @app.route("/contact")
